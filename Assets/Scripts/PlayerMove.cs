ee0e9abe888998280c885c090db47545775cd9f3
Jesús Soto <soleje2862004@gmail.com>
2025-10-17 23:51:14 -0600

    Parte 3 y 4

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -6,6 +7,16 @@ public class PlayerMove : MonoBehaviour
     public float jumpSpeed = 3;
 
     Rigidbody2D rb2D;
+
+    public bool betterJump = false;
+
+    public float fallMultiplier = 0.5f;
+
+    public float lowJumpMultiplier = 1f;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Animator animator;
 
     void Start()
     {
@@ -17,20 +28,49 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey("d")|| Input.GetKey("right"))
         {
             rb2D.linearVelocity = new Vector2(runSpeed, rb2D.linearVelocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
 
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2D.linearVelocity = new Vector2(-runSpeed, rb2D.linearVelocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
 
         }
         else
         {
             rb2D.linearVelocity = new Vector2(0, rb2D.linearVelocity.y);
+            animator.SetBool("Run", false);
         }
         if (Input.GetKey("space") && CheckGround.isGrounded)
         {
             rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpSpeed);
+        }
+
+        if (CheckGround.isGrounded == false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+        if (CheckGround.isGrounded == true)
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        if (betterJump)
+        {
+            if (rb2D.linearVelocity.y <0)
+            {
+                rb2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+
+            if (rb2D.linearVelocity.y > 0 && !Input.GetKey("space"))
+            {
+                rb2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
+
         }
     }
 }
